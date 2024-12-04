cdd789dc62621f4d99e641e9e427927ba47d4b8e
David Barth <76754802+barthdavid@users.noreply.github.com>
2024-12-04 01:33:27 +0100

    add part 2

--- day3/main.lua
+++ day3/main.lua
@@ -19,17 +19,74 @@ local function evalMul(str)
 	return a * b
 end
 
-local content = read("example.txt")
-local lines = content:split("\n")
+local function part_one(lines)
+	local total = 0
+	for _, line in ipairs(lines) do
+		local pattern = "mul%(%d+,%d+%)"
 
-local total = 0
-
-for _, line in ipairs(lines) do
-	local pattern = "mul%(%d+,%d+%)"
-
-	line.gsub(line, pattern, function(mul)
-		total = total + evalMul(mul)
-	end)
+		line.gsub(line, pattern, function(mul)
+			total = total + evalMul(mul)
+		end)
+	end
+	return total
 end
 
-print(total)
+local function insert_matches(t, s, pattern)
+	local i = 0
+	local j
+	while true do
+		i, j = string.find(s, pattern, i + 1)
+		if i == nil then
+			break
+		end
+
+		local match = string.sub(s, i, j)
+		t[i] = match
+	end
+end
+
+local function get_ops_table(line)
+	local ops = {}
+	local mul_pattern = "mul%(%d+,%d+%)"
+	local do_pattern = "do%(%)"
+	local dont_pattern = "don't%(%)"
+
+	insert_matches(ops, line, mul_pattern)
+	insert_matches(ops, line, do_pattern)
+	insert_matches(ops, line, dont_pattern)
+	return ops
+end
+
+local function part_two(lines)
+	local total = 0
+	local enabled = true
+	for _, line in ipairs(lines) do
+		local ops = get_ops_table(line)
+
+		local indexes = {}
+		for k in pairs(ops) do
+			table.insert(indexes, k)
+		end
+		table.sort(indexes)
+
+		for _, index in ipairs(indexes) do
+			if ops[index] == "do()" then
+				enabled = true
+			elseif ops[index] == "don't()" then
+				enabled = false
+			elseif enabled then
+				total = total + evalMul(ops[index])
+			end
+		end
+	end
+	return total
+end
+
+local content = read("example.txt")
+local lines = content:split("\n")
+print("part 1:", part_one(lines))
+
+content = read("example2.txt")
+lines = content:split("\n")
+
+print("part 2:", part_two(lines))
